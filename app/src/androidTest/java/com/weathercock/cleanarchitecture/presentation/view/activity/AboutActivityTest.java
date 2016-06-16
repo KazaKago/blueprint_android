cd7691450f052221ff473761d99f7dfa0aa71f9d
Kensuke Tamura <tamura_k@1923.co.jp>
2016-06-16 19:28:56 +0900

    add Espresso & Spoon Library. add simple screenshot test with spoon.

---
+++ app/src/androidTest/java/com/weathercock/cleanarchitecture/presentation/view/activity/AboutActivityTest.java
@@ -0,0 +1,36 @@
+package com.weathercock.cleanarchitecture.presentation.view.activity;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.squareup.spoon.Spoon;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * Created by tamura_k on 2016/06/16.
+ */
+public class AboutActivityTest {
+
+    @Rule
+    public ActivityTestRule<AboutActivity> activityRule = new ActivityTestRule<>(AboutActivity.class);
+
+    private AboutActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        activity = activityRule.getActivity();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testInitialState() throws Exception {
+        Spoon.screenshot(activity, "initial_state");
+    }
+
+}
