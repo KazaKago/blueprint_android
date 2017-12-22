76d70331fd677a03ab640712b079b8c7b7c11fe5
Kensuke Tamura <tamura_k@1923.co.jp>
2017-12-22 13:11:32 +0900

    split module.

---
+++ presentation/src/main/java/com/kazakago/cleanarchitecture/presentation/view/activity/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.kazakago.cleanarchitecture.presentation.view.activity
+
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.kazakago.cleanarchitecture.presentation.R
+import com.kazakago.cleanarchitecture.presentation.listener.activity.MainActivityViewModelListener
+import com.kazakago.cleanarchitecture.presentation.presenter.activity.MainActivityViewModel
+import com.kazakago.cleanarchitecture.presentation.view.fragment.MainFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), MainFragment.Listener, MainActivityViewModelListener {
+
+    companion object {
+        fun createIntent(context: Context): Intent = Intent(context, MainActivity::class.java)
+    }
+
+    private val viewModel by lazy { ViewModelProviders.of(this).get(MainActivityViewModel::class.java) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        lifecycle.addObserver(viewModel)
+        viewModel.listener = this
+
+        setSupportActionBar(toolbar)
+
+        if (savedInstanceState == null) {
+            replaceMainFragment()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModel.listener = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_about -> viewModel.onClickAboutMenu()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun replaceMainFragment() {
+        val fragment = MainFragment.createInstance()
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+
+    /* MainFragmentListener */
+
+    override fun setActionBarTitle(title: String?) {
+        supportActionBar?.title = title
+    }
+
+    /* MainActivityViewModelListener */
+
+    override fun toAboutActivity() {
+        val intent = AboutActivity.createIntent(this)
+        startActivity(intent)
+    }
+
+}
---
+++ presentation/src/main/java/com/kazakago/cleanarchitecture/presentation/view/fragment/MainFragment.kt
@@ -0,0 +1,110 @@
+package com.kazakago.cleanarchitecture.presentation.view.fragment
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Toast
+import com.kazakago.cleanarchitecture.presentation.R
+import com.kazakago.cleanarchitecture.presentation.extension.formattedDateTimeText
+import com.kazakago.cleanarchitecture.presentation.listener.fragment.MainFragmentViewModelListener
+import com.kazakago.cleanarchitecture.presentation.presenter.fragment.MainFragmentViewModel
+import com.kazakago.cleanarchitecture.presentation.view.adapter.CitySpinnerAdapter
+import com.kazakago.cleanarchitecture.presentation.view.adapter.ForecastRecyclerAdapter
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment(), MainFragmentViewModelListener {
+
+    interface Listener {
+        fun setActionBarTitle(title: String?)
+    }
+
+    companion object {
+        fun createInstance(): MainFragment = MainFragment()
+    }
+
+    private val viewModel by lazy { ViewModelProviders.of(this).get(MainFragmentViewModel::class.java) }
+    private lateinit var citySpinnerAdapter: CitySpinnerAdapter
+    private lateinit var forecastRecyclerAdapter: ForecastRecyclerAdapter
+    private var listener: Listener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = context as? Listener
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        lifecycle.addObserver(viewModel)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        viewModel.listener = this
+        fragmentManager
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        citySpinnerAdapter = CitySpinnerAdapter(activity!!)
+        citySpinner.adapter = citySpinnerAdapter
+
+        forecastRecyclerAdapter = ForecastRecyclerAdapter(activity!!)
+        forecastRecyclerAdapter.listener = viewModel
+        forecastRecyclerView.adapter = forecastRecyclerAdapter
+
+        viewModel.cityList.observe(this, Observer {
+            citySpinnerAdapter.cityList = it ?: listOf()
+            citySpinnerAdapter.notifyDataSetChanged()
+        })
+        viewModel.selectedPosition.observe(this, Observer {
+            it?.let {
+                citySpinner.setSelection(it)
+                listener?.setActionBarTitle(citySpinnerAdapter.cityList[it].name)
+            }
+        })
+        viewModel.weather.observe(this, Observer {
+            areaTextView.text = it?.location?.area
+            prefectureTextView.text = it?.location?.prefecture
+            cityTextView.text = it?.location?.city
+            publicTimeTextView.text = getString(R.string.public_time, it?.publicTime?.formattedDateTimeText(context!!) ?: "")
+            forecastRecyclerAdapter.forecastList = it?.forecasts ?: listOf()
+            forecastRecyclerAdapter.notifyDataSetChanged()
+        })
+        refreshButton.setOnClickListener {
+            viewModel.onClickRefresh()
+        }
+        citySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                viewModel.onCitySelected(position)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewModel.listener = null
+    }
+
+    /* MainFragmentViewModelListener */
+
+    override fun showToast(message: String?) {
+        Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
+    }
+
+}
