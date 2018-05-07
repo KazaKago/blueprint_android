f081268d2654d26de55560f0f4057c41a4cbe5e2
Kensuke Tamura <tamura_k@1923.co.jp>
2018-05-07 11:24:18 +0900

    add Chunk.

--- web/src/main/java/com/kazakago/cleanarchitecture/web/api/weather/WeatherRetrofit.kt
+++ web/src/main/java/com/kazakago/cleanarchitecture/web/api/weather/WeatherRetrofit.kt
@@ -1,11 +1,12 @@
 package com.kazakago.cleanarchitecture.web.api.weather
 
+import android.content.Context
 import com.kazakago.cleanarchitecture.web.api.RetrofitBuilder
 
 object WeatherRetrofit {
 
-    fun getWeatherApi(): WeatherApi {
-        return RetrofitBuilder("http://weather.livedoor.com/forecast/webservice/")
+    fun getWeatherApi(context: Context): WeatherApi {
+        return RetrofitBuilder(context, "http://weather.livedoor.com/forecast/webservice/")
                 .build()
                 .create(WeatherApi::class.java)
     }
--- web/src/main/java/com/kazakago/cleanarchitecture/web/repository/weather/WeatherApiRepositoryImpl.kt
+++ web/src/main/java/com/kazakago/cleanarchitecture/web/repository/weather/WeatherApiRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.kazakago.cleanarchitecture.web.repository.weather
 
+import android.content.Context
 import com.kazakago.cleanarchitecture.domain.model.city.CityId
 import com.kazakago.cleanarchitecture.domain.model.weather.Weather
 import com.kazakago.cleanarchitecture.domain.repository.weather.WeatherApiRepository
@@ -7,10 +8,10 @@ import com.kazakago.cleanarchitecture.web.api.weather.WeatherRetrofit
 import com.kazakago.cleanarchitecture.web.response.mapper.weather.WeatherResponseMapper
 import io.reactivex.Single
 
-class WeatherApiRepositoryImpl : WeatherApiRepository {
+class WeatherApiRepositoryImpl(private val context: Context) : WeatherApiRepository {
 
     override fun fetch(cityId: CityId): Single<Weather> {
-        val weatherApi = WeatherRetrofit.getWeatherApi()
+        val weatherApi = WeatherRetrofit.getWeatherApi(context)
         return weatherApi[cityId.value].map {
             WeatherResponseMapper.map(it)
         }
