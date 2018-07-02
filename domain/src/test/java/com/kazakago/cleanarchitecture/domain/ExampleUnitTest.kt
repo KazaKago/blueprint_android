6932ada28c4c721288cd6e508e044157f577e7d6
Kensuke Tamura <tamura_k@1923.co.jp>
2018-07-02 14:13:27 +0900

    add example test code in domain module.

---
+++ domain/src/test/java/com/kazakago/cleanarchitecture/domain/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.kazakago.cleanarchitecture.domain
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
