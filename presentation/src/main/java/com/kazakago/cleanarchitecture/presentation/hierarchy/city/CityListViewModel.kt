eaa4027d821287045997c6251f163a72e6975f1f
Kensuke Tamura <tamura_k@1923.co.jp>
2018-09-13 12:34:13 +0900

    update library.

--- presentation/src/main/java/com/kazakago/cleanarchitecture/presentation/hierarchy/city/CityListViewModel.kt
+++ presentation/src/main/java/com/kazakago/cleanarchitecture/presentation/hierarchy/city/CityListViewModel.kt
@@ -7,10 +7,11 @@ import com.kazakago.cleanarchitecture.domain.usecase.city.GetCityUseCase
 import com.kazakago.cleanarchitecture.presentation.livedata.liveevent.UnitLiveEvent
 import com.kazakago.cleanarchitecture.presentation.livedata.nonnulllivedata.NonNullLiveData
 import com.kazakago.cleanarchitecture.presentation.livedata.nonnulllivedata.NonNullLiveEvent
-import kotlinx.coroutines.experimental.DefaultDispatcher
-import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.android.Main
+import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.withContext
 
 class CityListViewModel(application: Application,
                         private val getCityUseCase: GetCityUseCase) : AndroidViewModel(application) {
@@ -32,9 +33,9 @@ class CityListViewModel(application: Application,
         toForecast.call(city)
     }
 
-    private fun fetchCityList() = launch(UI) {
+    private fun fetchCityList() = GlobalScope.launch(Dispatchers.Main) {
         try {
-            cityList.value = withContext(DefaultDispatcher) { getCityUseCase.execute(Unit) }
+            cityList.value = async(Dispatchers.Default) { getCityUseCase.execute(Unit) }.await()
         } catch (exception: Exception) {
             cityList.value = listOf()
             showToast.call(exception.localizedMessage)
--- presentation/src/main/java/com/kazakago/cleanarchitecture/presentation/hierarchy/forecast/ForecastViewModel.kt
+++ presentation/src/main/java/com/kazakago/cleanarchitecture/presentation/hierarchy/forecast/ForecastViewModel.kt
@@ -10,10 +10,11 @@ import com.kazakago.cleanarchitecture.domain.usecase.weather.GetWeatherUseCase
 import com.kazakago.cleanarchitecture.presentation.livedata.liveevent.UnitLiveEvent
 import com.kazakago.cleanarchitecture.presentation.livedata.nonnulllivedata.NonNullLiveData
 import com.kazakago.cleanarchitecture.presentation.livedata.nonnulllivedata.NonNullLiveEvent
-import kotlinx.coroutines.experimental.DefaultDispatcher
-import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.android.Main
+import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.withContext
 
 class ForecastViewModel(application: Application,
                         private val getWeatherUseCase: GetWeatherUseCase,
@@ -38,10 +39,10 @@ class ForecastViewModel(application: Application,
         showToast.call(forecast.telop)
     }
 
-    private fun fetchWeather() = launch(UI) {
+    private fun fetchWeather() = GlobalScope.launch(Dispatchers.Main) {
         isLoading.value = true
         try {
-            weather.value = withContext(DefaultDispatcher) { getWeatherUseCase.execute(city.id) }
+            weather.value = async(Dispatchers.Default) { getWeatherUseCase.execute(city.id) }.await()
         } catch (exception: Exception) {
             weather.value = null
             showToast.call(exception.localizedMessage)
