3d5ca8dab3bb249256022ca0f8bac8ff8eb7551f
Kensuke Tamura <tamura_k@1923.co.jp>
2018-10-09 10:56:53 +0900

    add compositeLiveDataOf.

---
+++ presentation/src/main/java/com/kazakago/cleanarchitecture/presentation/livedata/compositelivedata/CompositeLiveData.kt
@@ -0,0 +1,52 @@
+package com.kazakago.cleanarchitecture.presentation.livedata.compositelivedata
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import com.kazakago.cleanarchitecture.presentation.livedata.nonnulllivedata.NonNullLiveData
+import com.kazakago.cleanarchitecture.presentation.livedata.nonnulllivedata.NonNullObserver
+
+fun <A, B> compositeLiveDataOf(first: LiveData<A>, second: LiveData<B>) = PairLiveData(Pair(first, second))
+fun <A, B, C> compositeLiveDataOf(first: LiveData<A>, second: LiveData<B>, third: LiveData<C>) = TripleLiveData(Triple(first, second, third))
+
+class PairLiveData<A, B>(private val compositeLiveData: Pair<LiveData<A>, LiveData<B>>) : NonNullLiveData<Pair<A?, B?>>(Pair(compositeLiveData.first.value, compositeLiveData.second.value)) {
+
+    override fun observe(owner: LifecycleOwner, nonNullObserver: NonNullObserver<Pair<A?, B?>>) {
+        super.observe(owner, nonNullObserver)
+        compositeLiveData.first.observe(owner, Observer { setCurrentValue() })
+        compositeLiveData.second.observe(owner, Observer { setCurrentValue() })
+    }
+
+    override fun observeForever(nonNullObserver: NonNullObserver<Pair<A?, B?>>) {
+        super.observeForever(nonNullObserver)
+        compositeLiveData.first.observeForever { setCurrentValue() }
+        compositeLiveData.second.observeForever { setCurrentValue() }
+    }
+
+    private fun setCurrentValue() {
+        value = Pair(compositeLiveData.first.value, compositeLiveData.second.value)
+    }
+
+}
+
+class TripleLiveData<A, B, C>(private val compositeLiveData: Triple<LiveData<A>, LiveData<B>, LiveData<C>>) : NonNullLiveData<Triple<A?, B?, C?>>(Triple(compositeLiveData.first.value, compositeLiveData.second.value, compositeLiveData.third.value)) {
+
+    override fun observe(owner: LifecycleOwner, nonNullObserver: NonNullObserver<Triple<A?, B?, C?>>) {
+        super.observe(owner, nonNullObserver)
+        compositeLiveData.first.observe(owner, Observer { setCurrentValue() })
+        compositeLiveData.second.observe(owner, Observer { setCurrentValue() })
+        compositeLiveData.third.observe(owner, Observer { setCurrentValue() })
+    }
+
+    override fun observeForever(nonNullObserver: NonNullObserver<Triple<A?, B?, C?>>) {
+        super.observeForever(nonNullObserver)
+        compositeLiveData.first.observeForever { setCurrentValue() }
+        compositeLiveData.second.observeForever { setCurrentValue() }
+        compositeLiveData.third.observeForever { setCurrentValue() }
+    }
+
+    private fun setCurrentValue() {
+        value = Triple(compositeLiveData.first.value, compositeLiveData.second.value, compositeLiveData.third.value)
+    }
+
+}
---
+++ presentation/src/main/java/com/kazakago/cleanarchitecture/presentation/livedata/compositelivedata/NonNullCompositeLiveData.kt
@@ -0,0 +1,51 @@
+package com.kazakago.cleanarchitecture.presentation.livedata.compositelivedata
+
+import androidx.lifecycle.LifecycleOwner
+import com.kazakago.cleanarchitecture.presentation.livedata.nonnulllivedata.LateInitLiveData
+import com.kazakago.cleanarchitecture.presentation.livedata.nonnulllivedata.NonNullLiveData
+import com.kazakago.cleanarchitecture.presentation.livedata.nonnulllivedata.NonNullObserver
+
+fun <A, B> compositeLiveDataOf(first: LateInitLiveData<A>, second: LateInitLiveData<B>) = NonNullPairLiveData(Pair(first, second))
+fun <A, B, C> compositeLiveDataOf(first: LateInitLiveData<A>, second: LateInitLiveData<B>, third: LateInitLiveData<C>) = NonNullTripleLiveData(Triple(first, second, third))
+
+class NonNullPairLiveData<A, B>(private val compositeLiveData: Pair<LateInitLiveData<A>, LateInitLiveData<B>>) : NonNullLiveData<Pair<A, B>>(Pair(compositeLiveData.first.value, compositeLiveData.second.value)) {
+
+    override fun observe(owner: LifecycleOwner, nonNullObserver: NonNullObserver<Pair<A, B>>) {
+        super.observe(owner, nonNullObserver)
+        compositeLiveData.first.observe(owner, NonNullObserver { setCurrentValue() })
+        compositeLiveData.second.observe(owner, NonNullObserver { setCurrentValue() })
+    }
+
+    override fun observeForever(nonNullObserver: NonNullObserver<Pair<A, B>>) {
+        super.observeForever(nonNullObserver)
+        compositeLiveData.first.observeForever(NonNullObserver { setCurrentValue() })
+        compositeLiveData.second.observeForever(NonNullObserver { setCurrentValue() })
+    }
+
+    private fun setCurrentValue() {
+        value = Pair(compositeLiveData.first.value, compositeLiveData.second.value)
+    }
+
+}
+
+class NonNullTripleLiveData<A, B, C>(private val compositeLiveData: Triple<LateInitLiveData<A>, LateInitLiveData<B>, LateInitLiveData<C>>) : NonNullLiveData<Triple<A, B, C>>(Triple(compositeLiveData.first.value, compositeLiveData.second.value, compositeLiveData.third.value)) {
+
+    override fun observe(owner: LifecycleOwner, nonNullObserver: NonNullObserver<Triple<A, B, C>>) {
+        super.observe(owner, nonNullObserver)
+        compositeLiveData.first.observe(owner, NonNullObserver { setCurrentValue() })
+        compositeLiveData.second.observe(owner, NonNullObserver { setCurrentValue() })
+        compositeLiveData.third.observe(owner, NonNullObserver { setCurrentValue() })
+    }
+
+    override fun observeForever(nonNullObserver: NonNullObserver<Triple<A, B, C>>) {
+        super.observeForever(nonNullObserver)
+        compositeLiveData.first.observeForever(NonNullObserver { setCurrentValue() })
+        compositeLiveData.second.observeForever(NonNullObserver { setCurrentValue() })
+        compositeLiveData.third.observeForever(NonNullObserver { setCurrentValue() })
+    }
+
+    private fun setCurrentValue() {
+        value = Triple(compositeLiveData.first.value, compositeLiveData.second.value, compositeLiveData.third.value)
+    }
+
+}
