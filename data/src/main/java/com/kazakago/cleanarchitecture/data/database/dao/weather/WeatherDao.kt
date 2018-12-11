6d350dc8699b58a13733554846055540138836b0
KazaKago <weathercock_perch@yahoo.co.jp>
2018-12-12 08:25:49 +0900

    add room-coroutines

--- data/src/main/java/com/kazakago/cleanarchitecture/data/database/dao/weather/WeatherDao.kt
+++ data/src/main/java/com/kazakago/cleanarchitecture/data/database/dao/weather/WeatherDao.kt
@@ -10,39 +10,39 @@ import com.kazakago.cleanarchitecture.data.database.entity.weather.WeatherEntity
 interface WeatherDao {
 
     @Query("SELECT * FROM Weather WHERE city_id == (:cityId) LIMIT 1")
-    fun findWeather(cityId: String): WeatherEntity?
+    suspend fun findWeather(cityId: String): WeatherEntity?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(weather: WeatherEntity): Long
+    suspend fun insert(weather: WeatherEntity): Long
 
     @Delete
-    fun delete(weather: WeatherEntity): Int
+    suspend fun delete(weather: WeatherEntity): Int
 
     @Query("SELECT * FROM Location WHERE city_id == (:cityId) LIMIT 1")
-    fun findLocation(cityId: String): LocationEntity?
+    suspend fun findLocation(cityId: String): LocationEntity?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(location: LocationEntity): Long
+    suspend fun insert(location: LocationEntity): Long
 
     @Delete
-    fun delete(location: LocationEntity): Int
+    suspend fun delete(location: LocationEntity): Int
 
     @Query("SELECT * FROM Description WHERE city_id == (:cityId) LIMIT 1")
-    fun findDescription(cityId: String): DescriptionEntity?
+    suspend fun findDescription(cityId: String): DescriptionEntity?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(description: DescriptionEntity): Long
+    suspend fun insert(description: DescriptionEntity): Long
 
     @Delete
-    fun delete(description: DescriptionEntity): Int
+    suspend fun delete(description: DescriptionEntity): Int
 
     @Query("SELECT * FROM Forecast WHERE city_id == (:cityId)")
-    fun findForecasts(cityId: String): List<ForecastEntity>
+    suspend fun findForecasts(cityId: String): List<ForecastEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(forecasts: List<ForecastEntity>): List<Long>
+    suspend fun insert(forecasts: List<ForecastEntity>): List<Long>
 
     @Delete
-    fun delete(forecasts: List<ForecastEntity>): Int
+    suspend fun delete(forecasts: List<ForecastEntity>): Int
 
 }
--- data/src/main/java/com/kazakago/cleanarchitecture/data/repository/weather/WeatherRepositoryImpl.kt
+++ data/src/main/java/com/kazakago/cleanarchitecture/data/repository/weather/WeatherRepositoryImpl.kt
@@ -6,23 +6,21 @@ import com.kazakago.cleanarchitecture.data.database.mapper.weather.WeatherEntity
 import com.kazakago.cleanarchitecture.domain.model.city.CityId
 import com.kazakago.cleanarchitecture.domain.model.weather.Weather
 import com.kazakago.cleanarchitecture.domain.repository.weather.WeatherRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class WeatherRepositoryImpl(private val context: Context) : WeatherRepository {
 
     private val database = AppDatabase.create(context)
 
-    override suspend fun find(cityId: CityId): Weather = withContext(Dispatchers.IO) {
+    override suspend fun find(cityId: CityId): Weather {
         val weatherDao = database.weatherDao()
         val weather = weatherDao.findWeather(cityId.value)!!
         val location = weatherDao.findLocation(cityId.value)!!
         val description = weatherDao.findDescription(cityId.value)!!
         val forecasts = weatherDao.findForecasts(cityId.value)
-        WeatherEntityMapper.map(weather, location, description, forecasts)
+        return WeatherEntityMapper.map(weather, location, description, forecasts)
     }
 
-    override suspend fun insert(weather: Weather) = withContext<Unit>(Dispatchers.IO) {
+    override suspend fun insert(weather: Weather) {
         val weatherDao = database.weatherDao()
         val reverseMappingResult = WeatherEntityMapper.reverse(weather)
         weatherDao.insert(reverseMappingResult.weatherEntity)
@@ -31,7 +29,7 @@ class WeatherRepositoryImpl(private val context: Context) : WeatherRepository {
         weatherDao.insert(reverseMappingResult.forecastEntities)
     }
 
-    override suspend fun delete(weather: Weather) = withContext<Unit>(Dispatchers.IO) {
+    override suspend fun delete(weather: Weather) {
         val weatherDao = database.weatherDao()
         val reverseMappingResult = WeatherEntityMapper.reverse(weather)
         weatherDao.delete(reverseMappingResult.weatherEntity)
