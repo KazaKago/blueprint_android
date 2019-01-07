7c24686445238486b6f7f2d42038d5e4351d1221
Kensuke Tamura <tamura_k@1923.co.jp>
2019-01-07 12:19:11 +0900

    fix coroutine issue. https://qiita.com/kafumi/items/e8b9a74b59e00d5e2f3a

--- web/src/main/java/com/kazakago/cleanarchitecture/web/api/RetrofitBuilder.kt
+++ web/src/main/java/com/kazakago/cleanarchitecture/web/api/RetrofitBuilder.kt
@@ -1,7 +1,6 @@
 package com.kazakago.cleanarchitecture.web.api
 
 import android.content.Context
-import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.kazakago.cleanarchitecture.web.parser.MoshiBuilder
 import com.readystatesoftware.chuck.ChuckInterceptor
 import okhttp3.OkHttpClient
@@ -20,13 +19,10 @@ class RetrofitBuilder(context: Context, baseUrl: URL) {
 
     private val moshiConverter = MoshiConverterFactory.create(MoshiBuilder().build())
 
-    private val coroutineCallAdapter = CoroutineCallAdapterFactory()
-
     private val builder = Retrofit.Builder()
         .baseUrl(baseUrl.toString())
         .client(okHttpClient)
         .addConverterFactory(moshiConverter)
-        .addCallAdapterFactory(coroutineCallAdapter)
 
     fun build(): Retrofit {
         return builder.build()
--- web/src/main/java/com/kazakago/cleanarchitecture/web/api/weather/WeatherApi.kt
+++ web/src/main/java/com/kazakago/cleanarchitecture/web/api/weather/WeatherApi.kt
@@ -2,6 +2,7 @@ package com.kazakago.cleanarchitecture.web.api.weather
 
 import android.content.Context
 import com.kazakago.cleanarchitecture.web.api.RetrofitBuilder
+import com.kazakago.cleanarchitecture.web.extension.awaitBody
 import com.kazakago.cleanarchitecture.web.response.entity.weather.WeatherResponse
 import java.net.URL
 
@@ -9,10 +10,10 @@ class WeatherApi(context: Context) {
 
     private val apiService = RetrofitBuilder(context, URL("http://weather.livedoor.com/forecast/webservice/"))
         .build()
-        .create(WeatherService::class.java)!!
+        .create(WeatherService::class.java)
 
     suspend fun fetch(cityId: String): WeatherResponse {
-        return apiService.fetch(cityId).await()
+        return apiService.fetch(cityId).awaitBody()
     }
 
 }
