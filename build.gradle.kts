4d8fbd0006e3b6c1024bf407a1fc81379cb76bb5
Kensuke Tamura <tamura_k@1923.co.jp>
2019-08-26 12:36:37 +0900

    update kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.5.0")
-        classpath(kotlin("gradle-plugin", "1.3.41"))
+        classpath(kotlin("gradle-plugin", "1.3.50"))
         classpath("com.google.android.gms:oss-licenses-plugin:0.9.5")
 
         // NOTE: Do not place your application dependencies here; they belong
