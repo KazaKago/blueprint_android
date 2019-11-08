c3582d11ab356c53919345ad24aba272acb9b6b3
KazaKago <weathercock.cage@gmail.com>
2019-11-09 01:54:51 +0900

    delete unused import.

--- presentation/src/main/java/com/kazakago/cleanarchitecture/presentation/hierarchy/forecast/ForecastFragment.kt
+++ presentation/src/main/java/com/kazakago/cleanarchitecture/presentation/hierarchy/forecast/ForecastFragment.kt
@@ -8,7 +8,6 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.observe
 import com.kazakago.cleanarchitecture.domain.model.weather.Weather
-import com.kazakago.cleanarchitecture.presentation.R
 import com.kazakago.cleanarchitecture.presentation.databinding.FragmentForecastBinding
 import com.kazakago.cleanarchitecture.presentation.global.livedata.liveevent.observe
 import com.xwray.groupie.Group
