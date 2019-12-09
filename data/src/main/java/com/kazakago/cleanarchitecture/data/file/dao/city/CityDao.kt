1a460ad9b957a8d17d54799c323224c28635852e
Kensuke Tamura <tamura_k@1923.co.jp>
2019-12-09 10:31:26 +0900

    refactor json file input code.

--- data/src/main/java/com/kazakago/cleanarchitecture/data/file/dao/city/CityDao.kt
+++ data/src/main/java/com/kazakago/cleanarchitecture/data/file/dao/city/CityDao.kt
@@ -1,31 +1,21 @@
 package com.kazakago.cleanarchitecture.data.file.dao.city
 
 import android.content.Context
+import com.kazakago.cleanarchitecture.data.R
 import com.kazakago.cleanarchitecture.data.file.entity.city.PrefEntity
 import com.kazakago.cleanarchitecture.data.parser.MoshiBuilder
 import com.squareup.moshi.Types
-import java.io.BufferedReader
-import java.io.InputStreamReader
 
 class CityDao(private val context: Context) {
 
     fun find(): List<PrefEntity> {
-        val jsonStr = readJson()
+        val jsonStr = readJsonStr()
         return parseJson(jsonStr)
     }
 
-    private fun readJson(): String {
-        var text = ""
-        context.assets.open("json/city.json").use { inputStream ->
-            BufferedReader(InputStreamReader(inputStream)).use {
-                var str = it.readLine()
-                while (str != null) {
-                    text += str + "\n"
-                    str = it.readLine()
-                }
-            }
-        }
-        return text
+    private fun readJsonStr(): String {
+        val inputStream = context.resources.openRawResource(R.raw.city)
+        return inputStream.bufferedReader().use { it.readText() }
     }
 
     private fun parseJson(jsonStr: String): List<PrefEntity> {
