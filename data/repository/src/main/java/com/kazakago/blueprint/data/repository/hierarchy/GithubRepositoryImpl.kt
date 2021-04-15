2ba410b7653b8863c1f4bd2beb4ded9f1cb89d51
Kensuke Tamura <kazakago@gmail.com>
2021-04-15 19:29:10 +0900

    fix type.

--- data/repository/src/main/java/com/kazakago/blueprint/data/repository/hierarchy/GithubRepositoryImpl.kt
+++ data/repository/src/main/java/com/kazakago/blueprint/data/repository/hierarchy/GithubRepositoryImpl.kt
@@ -1,21 +1,20 @@
 package com.kazakago.blueprint.data.repository.hierarchy
 
 import com.kazakago.blueprint.data.api.hierarchy.GithubService
+import com.kazakago.blueprint.data.cache.hierarchy.GithubCache
 import com.kazakago.blueprint.data.mapper.entity.github.GithubOrgEntityMapper
 import com.kazakago.blueprint.data.mapper.entity.github.GithubRepoEntityMapper
 import com.kazakago.blueprint.data.mapper.response.github.GithubOrgResponseMapper
 import com.kazakago.blueprint.data.mapper.response.github.GithubRepoResponseMapper
-import com.kazakago.blueprint.data.cache.hierarchy.GithubCache
 import com.kazakago.blueprint.data.repository.flowable.GithubOrgsResponder
 import com.kazakago.blueprint.data.repository.flowable.GithubReposResponder
 import com.kazakago.blueprint.domain.model.hierarchy.github.GithubOrg
 import com.kazakago.blueprint.domain.model.hierarchy.github.GithubOrgName
 import com.kazakago.blueprint.domain.model.hierarchy.github.GithubRepo
 import com.kazakago.blueprint.domain.repository.hierarchy.GithubRepository
-import com.kazakago.storeflowable.core.State
+import com.kazakago.storeflowable.core.FlowableState
 import com.kazakago.storeflowable.core.mapContent
 import com.kazakago.storeflowable.pagination.create
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GithubRepositoryImpl @Inject constructor(
@@ -27,7 +26,7 @@ class GithubRepositoryImpl @Inject constructor(
     private val githubRepoEntityMapper: GithubRepoEntityMapper,
 ) : GithubRepository {
 
-    override fun followOrgs(): Flow<State<List<GithubOrg>>> {
+    override fun followOrgs(): FlowableState<List<GithubOrg>> {
         val githubOrgsFlowable = GithubOrgsResponder(githubService, githubCache, githubOrgResponseMapper).create()
         return githubOrgsFlowable.publish().mapContent {
             it.map { githubOrgEntity -> githubOrgEntityMapper.map(githubOrgEntity) }
@@ -44,7 +43,7 @@ class GithubRepositoryImpl @Inject constructor(
         githubOrgsFlowable.requestAddition(continueWhenError)
     }
 
-    override fun followRepos(githubOrgName: GithubOrgName): Flow<State<List<GithubRepo>>> {
+    override fun followRepos(githubOrgName: GithubOrgName): FlowableState<List<GithubRepo>> {
         val githubReposFlowable = GithubReposResponder(githubService, githubCache, githubRepoResponseMapper, githubOrgName).create()
         return githubReposFlowable.publish().mapContent {
             it.map { githubRepoEntity -> githubRepoEntityMapper.map(githubRepoEntity) }
--- domain/repository/src/main/java/com/kazakago/blueprint/domain/repository/hierarchy/GithubRepository.kt
+++ domain/repository/src/main/java/com/kazakago/blueprint/domain/repository/hierarchy/GithubRepository.kt
@@ -3,18 +3,17 @@ package com.kazakago.blueprint.domain.repository.hierarchy
 import com.kazakago.blueprint.domain.model.hierarchy.github.GithubOrg
 import com.kazakago.blueprint.domain.model.hierarchy.github.GithubOrgName
 import com.kazakago.blueprint.domain.model.hierarchy.github.GithubRepo
-import com.kazakago.storeflowable.core.State
-import kotlinx.coroutines.flow.Flow
+import com.kazakago.storeflowable.core.FlowableState
 
 interface GithubRepository {
 
-    fun followOrgs(): Flow<State<List<GithubOrg>>>
+    fun followOrgs(): FlowableState<List<GithubOrg>>
 
     suspend fun refreshOrgs()
 
     suspend fun requestAdditionalOrgs(continueWhenError: Boolean)
 
-    fun followRepos(githubOrgName: GithubOrgName): Flow<State<List<GithubRepo>>>
+    fun followRepos(githubOrgName: GithubOrgName): FlowableState<List<GithubRepo>>
 
     suspend fun refreshRepos(githubOrgName: GithubOrgName)
 
