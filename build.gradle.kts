f9e4af1228fec397cb0fc2429cb0ea0260068519
Kensuke Tamura <kazakago@gmail.com>
2021-05-14 10:26:21 +0900

    update AGP 4.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0")
+        classpath("com.android.tools.build:gradle:4.2.1")
         classpath(kotlin("gradle-plugin", "1.5.0"))
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.35.1")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.4")
