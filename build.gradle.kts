500b5aa7b81498d3f3870c9a7cc24233637f1807
Kensuke Tamura <kazakago@gmail.com>
2021-09-22 15:08:16 +0900

    update dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.2")
-        classpath(kotlin("gradle-plugin", version = "1.5.30"))
-        classpath(kotlin("serialization", version = "1.5.30"))
+        classpath(kotlin("gradle-plugin", version = "1.5.31"))
+        classpath(kotlin("serialization", version = "1.5.31"))
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.4")
 
