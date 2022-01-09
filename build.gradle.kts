34b5d8cf5f94362b34aae2d9c044173444aa568c
Kensuke Tamura <kazakago@gmail.com>
2022-01-10 01:03:09 +0900

    migrate gradle plugins syntax.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
-        classpath(kotlin("gradle-plugin", version = "1.6.10"))
-        classpath(kotlin("serialization", version = "1.6.10"))
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle.kts files
-    }
+plugins {
+    id("com.android.application") version "7.0.4" apply false
+    id("com.android.library") version "7.0.4" apply false
+    kotlin("android") version "1.6.10" apply false
+    kotlin("plugin.serialization") version "1.6.10" apply false
+    id("dagger.hilt.android.plugin") version "2.40.5" apply false
 }
 
 tasks.register("clean", Delete::class) {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,17 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "dagger.hilt.android.plugin") {
+                useModule("com.google.dagger:hilt-android-gradle-plugin:${requested.version}")
+            }
+        }
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
