4fbfd9b04bb8ad0c3db73426fba91afa32c0bc5f
Kensuke Tamura <kazakago@gmail.com>
2022-03-01 16:58:14 +0900

    migrate hilt 2.41

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.android.library") version "7.1.2" apply false
     kotlin("android") version "1.6.10" apply false
     kotlin("plugin.serialization") version "1.6.10" apply false
-    id("dagger.hilt.android.plugin") version "2.41" apply false
+    id("com.google.dagger.hilt.android") version "2.41" apply false
 }
 
 tasks.register("clean", Delete::class) {
