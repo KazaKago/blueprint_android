2e7a149b6c34b94e2e95ac1adec03c21a01e1270
Kensuke Tamura <kazakago@gmail.com>
2022-04-07 01:33:24 +0900

    fix project structure.

--- data/cache/build.gradle.kts
+++ data/cache/build.gradle.kts
@@ -1,19 +1,41 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 @Suppress("DSL_SCOPE_VIOLATION") // https://youtrack.jetbrains.com/issue/KTIJ-19369
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.dagger.hilt)
 }
 
-java {
-    setSourceCompatibility(libs.versions.java.get())
-    setTargetCompatibility(libs.versions.java.get())
-}
-
-tasks.withType<KotlinCompile> {
+android {
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    flavorDimensions += "app"
+    productFlavors {
+        create("production") {
+            dimension = "app"
+        }
+        create("develop") {
+            dimension = "app"
+        }
+    }
+    compileOptions {
+        sourceCompatibility(libs.versions.java.get())
+        targetCompatibility(libs.versions.java.get())
+    }
     kotlinOptions {
         jvmTarget = libs.versions.java.get()
+        freeCompilerArgs = freeCompilerArgs + "-opt-in=kotlinx.coroutines.FlowPreview"
     }
 }
 
@@ -21,7 +43,7 @@ dependencies {
     // Kotlinx DateTime
     implementation(libs.kotlinx.datetime)
     // Dagger
-    implementation(libs.dagger.hilt.core)
+    implementation(libs.dagger.hilt.android)
     kapt(libs.dagger.hilt.compiler)
     // StoreFlowable.kt
     implementation(libs.storeflowable)
