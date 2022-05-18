b2378145d0aa7bd051f2a4115a15566156565072
KazaKago <kensuke_tamura@r.recruit.co.jp>
2022-05-18 14:55:49 +0900

    remove unused flag

--- presentation/controller/build.gradle.kts
+++ presentation/controller/build.gradle.kts
@@ -36,7 +36,6 @@ android {
     }
     kotlinOptions {
         jvmTarget = libs.versions.java.get()
-        freeCompilerArgs = freeCompilerArgs + "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
     }
     buildFeatures {
         compose = true
