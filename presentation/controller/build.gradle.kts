16f5ce23b3b218c12fe12c7307d1d32f1b6515e9
Kensuke Tamura <kazakago@gmail.com>
2022-06-30 11:51:23 +0900

    update dependencies

--- presentation/controller/build.gradle.kts
+++ presentation/controller/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.get()
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
     libraryVariants.all {
         kotlin.sourceSets {
